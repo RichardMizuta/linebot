56607d82c761f863a7e74a2dd5f7c4b24edb1ed1
noahmizuta <noahmizuta@gmail.com>
2023-02-08 03:01:20 +0900

    Improved

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const server = require("express")();
 const line = require("@line/bot-sdk");
-const { response } = require("express");
-const a = 2;
 
 //環境設定
+
 const line_config = {
     channelAccessToken: process.env.LINE_ACCESS_TOKEN,
     channelSecret: process.env.LINE_CHANNEL_SECRET,
